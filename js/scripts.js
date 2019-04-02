2f7e26dfd5aacd65a063ee5624f740a2d4a9d614
hannah melendy and Kaya Jepson <kaya@>
2019-04-02 11:22:31 -0700

    remove excess code

--- js/scripts.js
+++ js/scripts.js
@@ -2,29 +2,19 @@ $(document).ready(function() {
   $("#formOne").submit(function(event) {
     var sentences = $("input#input1").val();
     var letters = sentences.split("");
-    // console.log(letters);
-    // if (incrementNum === ' ' || incrementNum < 0 || incrementNum > ceilingNum || (isNaN(incrementNum)) || ceilingNum === ' ' || ceilingNum < 0 || ceilingNum > ceilingNum || (isNaN(ceilingNum)) ) {
-    // alert("Please try again!");
-    // }
 
     newArray = [];
     letters.forEach(function(letter){
-      // var userInput = $("input1#");
       if (letter === 'a' || letter ===  'e' || letter === 'i' || letter === 'o' ||letter ===  'u') {
         letter = "-";
         newArray.push(letter);
       } else {
         newArray.push(letter);
       }
-      console.log(newArray);
+      // console.log(newArray);
       var combine = newArray.join('');
       console.log(combine);
     })
-
-    //
-    // for (var index = 0; index <= ceilingNum; index += incrementNum) {
-    //   console.log(index);
-    // }
     event.preventDefault();
   });
 });
