f921ab9df40acf95304e7d54922c719fc906f94a
hannah melendy and Kaya Jepson <kaya@>
2019-04-02 11:10:30 -0700

    add current point of trying to substitute dashes for vowels AND retain other letters

--- js/scripts.js
+++ js/scripts.js
@@ -2,17 +2,22 @@ $(document).ready(function() {
   $("#formOne").submit(function(event) {
     var sentences = $("input#input1").val();
     var letters = sentences.split("");
-    console.log(letters);
+    // console.log(letters);
     // if (incrementNum === ' ' || incrementNum < 0 || incrementNum > ceilingNum || (isNaN(incrementNum)) || ceilingNum === ' ' || ceilingNum < 0 || ceilingNum > ceilingNum || (isNaN(ceilingNum)) ) {
     // alert("Please try again!");
     // }
-    // sentences.forEach(function(letter){
-    //   var userInput = $("input1#");
-    //   if (letter === 'a' || 'e' || 'i' || 'o' || 'u') {
-    //     letter = "-";
-    //   }
-    //   console.log(userInput);
-    // })
+
+    newArray = [];
+    letters.forEach(function(letter){
+      // var userInput = $("input1#");
+      if (letter === 'a' || 'e' || 'i' || 'o' || 'u') {
+        letter = "-";
+        newArray.push(letter[1]);
+      } else {
+        newArray.push(letter[1]);
+      }
+      console.log(letter);
+    })
     //
     // for (var index = 0; index <= ceilingNum; index += incrementNum) {
     //   console.log(index);
